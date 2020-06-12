869d7ba88a34aa367b50041fe8e3a2e8b1e94f0c
Oleg Nedbaylo <olned64@gmail.com>
2020-06-12 13:12:08 +0000

    subtrac removed from lib

--- src/arithmetic.cpp
+++ src/arithmetic.cpp
@@ -5,7 +5,3 @@ int add(int i, int j)
     return i + j;
 }
 
-int subtrac(int i, int j)
-{
-    return i - j;
-}
--- tests/arithmetic_test.cpp
+++ tests/arithmetic_test.cpp
@@ -6,9 +6,3 @@ TEST_CASE("2 + 3 == 5", "[add]")
 {
     REQUIRE(add(2, 3) == 5);
 }
-
-
-TEST_CASE("5 - 3 == 2", "[subtrac]")
-{
-    REQUIRE(subtrac(5, 3) == 2);
-}

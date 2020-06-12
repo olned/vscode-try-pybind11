5241207e491efe9f3020870797010eb3b75f1c53
Oleg Nedbaylo <olned64@gmail.com>
2020-06-12 13:48:52 +0000

    template <class AnimalBase = Animal>...

--- bindings/py_animal.hpp
+++ bindings/py_animal.hpp
@@ -2,24 +2,10 @@
 #include <pet/animal.hpp>
 #include <pybind11/pybind11.h>
 
-class PyAnimal : public Animal {
+template <class AnimalBase = Animal>
+class PyAnimal : public AnimalBase {
 public:
-  /* Inherit the constructors */
-  using Animal::Animal;
-
-  /* Trampoline (need one for each virtual function) */
-  std::string go(int n_times) override
-  {
-    PYBIND11_OVERLOAD_PURE(
-        std::string, /* Return type */
-        Animal,      /* Parent class */
-        go,          /* Name of function in C++ (must match Python name) */
-        n_times      /* Argument(s) */
-    );
-  }
-
-  std::string name() override
-  {
-    PYBIND11_OVERLOAD(std::string, Animal, name, );
-  }
+  using AnimalBase::AnimalBase; // Inherit constructors
+  std::string go(int n_times) override { PYBIND11_OVERLOAD_PURE(std::string, AnimalBase, go, n_times); }
+  std::string name() override { PYBIND11_OVERLOAD(std::string, AnimalBase, name, ); }
 };
